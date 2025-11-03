995b36764f47840e26f672ef6b9698fb3f52f474
choiao0 <qawse6@daum.net>
2025-11-03 17:37:52 +0900

    test(WinningNumbersTest): 당첨 번호 예외 테스트 코드 추가

---
+++ src/test/java/lotto/domain/WinningNumbersTest.java
@@ -0,0 +1,38 @@
+package lotto.domain;
+
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+
+public class WinningNumbersTest {
+    @ParameterizedTest
+    @ValueSource(strings = {"0,1,2,3,4,5", "1,2,3,4,5,46", "-1,1,2,3,4,5"})
+    void 당첨_번호가_1부터_45_사잇값이_아니면_예외가_발생한다(String numbers) {
+        assertThatThrownBy(() -> new WinningNumbers(numbers))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("[ERROR] 당첨 번호는 1과 45 사이여야 합니다.");
+    }
+
+    @Test
+    void 당첨_번호가_6개가_아니면_예외가_발생한다() {
+        assertThatThrownBy(() -> new WinningNumbers("1,2,3,4,5,6,7"))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("[ERROR] 당첨 번호는 6개여야 합니다.");
+    }
+
+    @Test
+    void 당첨_번호에_숫자와_쉼표_외의_다른_문자가_있으면_예외가_발생한다() {
+        assertThatThrownBy(() -> new WinningNumbers("1.2.3.4.5.6"))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("[ERROR] 당첨 번호는 6개여야 합니다.");
+    }
+
+    @Test
+    void 당첨_번호에_중복된_숫자가_있으면_예외가_발생한다() {
+        assertThatThrownBy(() -> new WinningNumbers("1,2,3,4,5,5"))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("[ERROR] 당첨 번호는 중복될 수 없습니다.");
+    }
+}
